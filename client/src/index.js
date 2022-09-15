212675ac3f3b8fb502c2a67a799cc0b081622b54
Shahrukh1505 <shahrukhansari15@gmail.com>
2022-09-15 19:52:02 +0530

    look bitch

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom';
+import  ReactDOM  from 'react-dom/client';
 import App from './App';
 import {Provider} from 'react-redux';
 import {createStore,applyMiddleware, compose} from 'redux';
@@ -7,11 +7,10 @@ import  thunk from 'redux-thunk';
 import reducers from './reducers';
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDOM.render(
-  <Provider store={store}>
-      <App/>,
-  </Provider>, document.getElementById('root'),
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Provider store={store}>
+  <App/>,
+</Provider>);
 
 
 // If you want to start measuring performance in your app, pass a function
