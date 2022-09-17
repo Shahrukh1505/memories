71e381202d7cb100a95bad577e0a0d2d4f81d7ce
Shahrukh1505 <shahrukhansari15@gmail.com>
2022-09-17 16:16:21 +0530

    hehe

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
-import React,{useState, useEffect} from "react";
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import { useDispatch } from "react-redux";
+import React from "react";
+import {Container} from '@material-ui/core';
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import {getPosts} from './actions/posts'
-import memories from './images/memories.png';
-import Form from './components/Form/Form';
-import Posts from './components/Posts/Posts';
-import useStyles from './styles';
 import { Home } from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+
+
 const App = () =>{
     
     return (
@@ -20,7 +16,7 @@ const App = () =>{
         <Route path="/" exact component={Home}></Route>
         <Route path="/auth" exact component={Auth}></Route>
       </Switch>
-      <Home></Home>
+     
     </Container>
     </BrowserRouter>
    )
--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,8 +1,50 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import {Avatar, Button, Paper, Grid, Typography, Container} from '@material-ui/core';
+import Input from './Input.js';
+import useStyles from './styles';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 const Auth = () => {
+  const classes = useStyles();
+  const isSignUp = false;
+  const [showPassword, setShowPassword] = useState(false);
+  
+  const handleSubmit = () => {
+
+  }
+  const handleChange = () => {
+
+  }
+  const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+
+ 
   return (
-    <div>Auth</div>
+    <Container component = "main" maxWidth="xs">
+      <Paper className = {classes.paper} elevation={3}>
+        <Avatar className = {classes.avatar}>
+          <LockOutlinedIcon/>
+          </Avatar>
+          <Typography variant="h5">{isSignUp ? 'Sign Up' : 'Sign in'}</Typography>
+          <form className={classes.form} onSubmit={handleSubmit}>
+           <Grid container spacing={2}>
+{
+   isSignUp && (
+    <>
+        <Input name='firstName' handleChange={handleChange} label='First Name' autoFocus half/>
+        <Input name='lastName' handleChange={handleChange} label='Last Name' half/>
+        
+    </>
+)
+}
+<Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+<Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+{isSignUp && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange}  type="password"></Input>}
+            </Grid>
+            <Button type="submit" fullWidth variant="contained" color ="primary" className = {classes.submit}>
+              {isSignUp ? 'Sign Up' : 'Sign In'}
+            </Button>
+          </form>
+      </Paper>
+    </Container>
   )
 }
 
